9d4cfa28d74889746768329536e4458e48842c51
Bencic <aleksander.bencic@nib.si>
2023-06-26 14:17:17 +0200

    filtered

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 print("Hello")
 
+import fnmatch
+import glob
 import os
 
 data_folder_name = "data"
-final_merged_file = "merged_file.txt"
+filter_file_name = "file*"
+final_merged_file = "results\merged_file.txt"
 remove_empty_lines = True
 
 
-def get_absolute_file_paths(folder_name):
+def get_absolute_file_paths(folder_name: str, filter: str = "*") -> list[str]:
     absolute_paths = []
     for dirpath, _, filenames in os.walk(folder_name):
         for filename in filenames:
-            absolute_path = os.path.abspath(os.path.join(dirpath, filename))
-            absolute_paths.append(absolute_path)
+            if fnmatch.fnmatch(filename, filter):
+                absolute_path = os.path.abspath(os.path.join(dirpath, filename))
+                absolute_paths.append(absolute_path)
     return absolute_paths
 
 
@@ -40,7 +44,7 @@ def writ_list_to_text_file(file_data: list[str], file_path: str) -> None:
 
 # združimo obe datoteke in jih shranimo n+ v novo datoteko imenova skupni.txt
 
-all_files_paths = get_absolute_file_paths(data_folder_name)
+all_files_paths = get_absolute_file_paths(data_folder_name, filter_file_name)
 merged_list = []
 for path in all_files_paths:
     file_data = read_text_file_to_list(path)
